c87bf2ae8a676c8ff8efc5c1dc15b35943c86ba9
laurirasanen <lauri@rasanen.dev>
2022-07-02 22:01:39 +0300

    don't need these :)

--- tests/test_export.py
+++
@@ -1,9 +0,0 @@
-import bpy
-from io_scene_md3.export_md3 import MD3Exporter
-
-
-def test_export_doesnt_crash(tmpdir, simple_blend):
-    fname = tmpdir / 'output.md3'
-    MD3Exporter(bpy.context)(str(fname))
-    assert fname.exists()
-    assert fname.stat().st_size > 0
